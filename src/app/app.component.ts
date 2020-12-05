4a3d102b962dd0100f9fe3310eeaa7ae6868ad5e
Alvaro-Osorio <alvaroosorioiba@gmail.com>
2020-12-05 17:50:02 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  titulo = 'app Calculadora';
+  numeroA:number;
+  numeroB:number;
+  resultado:number;
+
+  sumarNumeros():void{
+    this.resultado = this.numeroA + this.numeroB;
+  }
+  multiplicarNumeros():void{
+    this.resultado = this.numeroA * this.numeroB;
+  }
+  restarNumeros():void{
+    this.resultado = this.numeroA - this.numeroB;
+  }
+  dividirNumeros():void{
+    this.resultado = this.numeroA / this.numeroB;
+  }
 }
